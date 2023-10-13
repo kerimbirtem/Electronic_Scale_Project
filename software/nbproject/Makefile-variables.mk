aed1d86b9b2e6dddfa53b7aaa7a88064d6c5b31e
Kerim Birtem <118342181+kerimbirtem@users.noreply.github.com>
2023-10-13 23:59:42 +0300

    Add files via upload

---
+++ software/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=scale-soft.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/scale-soft.production.hex
